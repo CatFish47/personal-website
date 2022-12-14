68b14c55e39ce7f6b17933009ecb31e7d5d0dd9d
Grant Cheng <chengxgrant@gmail.com>
2022-12-14 14:03:18 -0800

    finished projects feature

--- src/components/ProjectsPage/projects.ts
+++ src/components/ProjectsPage/projects.ts
@@ -28,7 +28,8 @@ export const featuredProjects: Project[] = [
     description: "Reinforcement learning with car racing",
     details:
       "Explored with 3 other teammates different model-free reinforcement learning algorithms. Through the use of Open AI Gym's custom car racing library, we trained various reinforcement learning algorithms to control a car to drive through a racetrack by taking images of each frame and using the pixels of the image as an input to determine the car's next move.",
-    image: "",
+    image:
+      "https://github.com/acmucsd-projects/KachowRL/blob/main/gifs/ppo_results.gif?raw=true",
     when: "Winter 2022",
     links: [
       {
@@ -43,7 +44,7 @@ export const featuredProjects: Project[] = [
     description: "Using the genetic algorithm to play arcade games",
     details:
       "To dive into the basics of machine learning, my partner and I explored how the genetic algorithm worked and applied it to play a game of snake. The end result was a battle-royale-style snake game, each one using a genetic-algorithm-trained neural network to fight to the death. To show that we could apply what we learned to not just snake, we also trained a neural network to play the arcade game Atari Breakout as well.",
-    image: "",
+    image: "/portrait-square.jpg",
     when: "Summer 2021",
     links: [
       {
@@ -63,7 +64,7 @@ export const featuredProjects: Project[] = [
     description: "A personal website built using Astro",
     details:
       "Motivated from both a need for a personal website and a desire to learn the web framework Astro, I built a website entirely with Astro (with the exception of the occasional React components).",
-    image: "",
+    image: "/portrait-square.jpg",
     when: "December 2022",
     links: [
       {
